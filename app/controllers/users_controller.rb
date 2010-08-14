a8da6b0e51ed8a05b286aeb51ac1090cf18dd572
Michael Hartl <michael@michaelhartl.com>
2010-08-13 18:11:14 -0700

    foo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   # GET /users.xml
   def index
     @users = User.all
+  
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
   end
 
   # GET /users/1
